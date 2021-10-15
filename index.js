091b957fdf3a18964ee74da81ce561fa4ea1a2fd
rhvsingh <singhrajaharshvardhan004@gmail.com>
2021-10-15 18:30:08 +0530

    Changes made in theme

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-var gamePattern=[];
-var userClickedPattern=[];
-var buttonColours=["red","blue","green","yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
 
 var level = 0;
-var started=false;
+var started = false;
 let score = 0;
 
 $(document).keypress(function() {
 
-  if (!started) {
-     level--; // fixes level display on game start //
-    toggleTurn(false);
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-  }
+    if (!started) {
+        level--; // fixes level display on game start //
+        toggleTurn(false);
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
 });
 
 $(".btn").click(function() {
 
-  var userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
 
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 
-  checkAnswer(userClickedPattern.length-1);
-  toggleTurn(false);
+    checkAnswer(userClickedPattern.length - 1);
+    toggleTurn(false);
 });
 
 
@@ -34,63 +34,63 @@ $(".btn").click(function() {
 function checkAnswer(currentLevel) {
 
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-      if (userClickedPattern.length === gamePattern.length){
-        setTimeout(function () {
-          nextSequence();
-        }, 1000);
-      }
-      score = score + 1;
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+        score = score + 1;
     } else {
-      playSound("wrong");
-      $("body").addClass("game-over");
-      
-      $("#level-title").text(`Your Score : ${score} (Press Any Key To Restart)`)
-      score = 0;
-      setTimeout(function () {
-        $("body").removeClass("game-over");
-      }, 200);
-      
-      startOver();
+        playSound("wrong");
+        $("body").addClass("game-over");
+
+        $("#level-title").text(`Your Score : ${score} (Press Any Key To Restart)`)
+        score = 0;
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        startOver();
     }
 }
 
 
 function nextSequence() {
-  userClickedPattern = [];
-  level++;
-  $("#level-title").text("Level " + level);
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
 
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColour);
-  toggleTurn()
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+    toggleTurn()
 }
 
 function animatePress(currentColor) {
-  $("#" + currentColor).addClass("pressed");
-  setTimeout(function () {
-    $("#" + currentColor).removeClass("pressed");
-  }, 100);
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
 }
 
 function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 }
 
 function startOver() {
-  level = 0;
-  gamePattern = [];
-  started = false;
-  $('#turn-indicator').hide();
+    level = 0;
+    gamePattern = [];
+    started = false;
+    $('#turn-indicator').hide();
 }
 
 function toggleTurn(player = true) {
-  let text="Memorize the pattern";
-  if (player) {
-     text = "Your turn";
-  }
-  $("#turn-indicator").text(text);
+    let text = "Memorize the pattern";
+    if (player) {
+        text = "Your turn";
+    }
+    $("#turn-indicator").text(text);
 }
